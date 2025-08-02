d5cd186bb7669e62bc36e72fe16d376f23d20b03
TrevisanFabby <fabricialondrina@hotmail.com>
2025-08-02 15:38:36 -0300

    Adicionando programa de calculadora
    Calculadora com operações simples, raiz quadrada e portência.

---
+++ calculadora_com_def.py
@@ -0,0 +1,75 @@
+import math
+
+def raiz_quadrada(num):
+    return math.sqrt(num)
+
+def potencia(num,num2):
+     return math.pow(num,num2)
+
+def soma(num, num2):
+     return num+num2
+
+def subtracao(num, num2):
+     return num-num2
+
+def multiplicacao(num, num2):
+     return num*num2
+
+def divisao(num, num2):
+     return num/num2
+
+
+while True:
+    calcular = input("Olá, bem vindo a nossa calculadora! \n\nInforme a ação desejada:\n\n" \
+    "   'C' = continuar e \n" \
+    "   'S' = sair: ")  
+    
+    if calcular.upper() == 'S':
+        break
+    elif calcular.upper()!= 'S' and calcular.upper() != 'C':
+        print("Opção inválida.")
+        break
+    else:
+        try:
+            n1 = float(input("Digite N1: "))
+            operacao = input("Escolha operação: +, -, *, /, r (raiz quadrada), p (potência)")
+            
+            if operacao.upper() == 'R':
+                resultado = raiz_quadrada(n1)
+                print (f"√{n1} = {resultado}.")
+
+            elif operacao.upper() == 'P':
+                n2 = int(input("digite expoente: "))
+                resultado = potencia(n1,n2)
+                print (f"{n1} ^ {n2} = {resultado}")
+
+            else:
+                try:
+                    n2 = float(input("digite N2: "))
+                    if operacao == '+':
+                        resultado = soma(n1, n2)
+                        print(f"{n1} + {n2} = {resultado}")
+
+                    elif operacao =='-':
+                        resultado = subtracao(n1, n2)
+                        print(f"{n1} - {n2} = {resultado}")
+
+                    elif operacao == '*':
+                        resultado = multiplicacao(n1, n2)
+                        print(f"{n1} * {n2} = {resultado}")
+
+                    elif operacao == '/':
+                        resultado = divisao(n1, n2)
+                        print(f"{n1} / {n2} = {round(resultado,2)}")
+
+                    else:
+                        print("Operação inválida!")
+                        
+                except Exception:
+                    print("Erro try operação")
+                    break
+        except Exception:
+                print("Erro Try números!")
+                break
+
+    print("\n")
